72d59c5551b4158241992e3cf2b362204889afeb
Tereza <bisharyant>
2024-06-05 17:31:46 +0400

    c34

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 module "s3" {
   source = "./modules/s3"
-  count = var.enable_s3 ? 1 : 0
+  //count = var.enable_s3 ? 1 : 0
+  count = module.wrapper.so == "some_value" ? 1 : 0
 # >>>Lambda portion
  	#trigger_lambda = true
   #lf_arn          = module.lambda.lf_arn
@@ -13,6 +14,7 @@ module "s3" {
 
 module "lambda" {
   source = "./modules/lambda_function"
-  count = var.enable_lf == true ? 1 : 0
+  count = enable_lf == true ? 1 : 0
+  //bucket_arn = module.wrapper.so == "some_val" ? sdsd : sdsd
   //bucket_arn = module.s3.bucket_arn
 }
