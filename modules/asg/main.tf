e88ad5718d90fcde091f23f0fd4a2dd983d66b3d
Tereza <bisharyant>
2024-06-07 23:23:20 +0400

    c53

--- modules/asg/main.tf
+++ modules/asg/main.tf
@@ -5,6 +5,7 @@ resource "aws_autoscaling_group" "asg" {
 	desired_capacity = 3
 	//health_check_type = "EC2"
 	//force_delete = false
+	target_group_arns = [aws_lb_target_group.target_group.arn]
 	vpc_zone_identifier = [var.pub_sub_a_id, var.pub_sub_b_id]
 	
 	launch_template {
