e4f338151feaa5098e8ea9ea6f6c775c9ef41d23
Tereza <bisharyant>
2024-06-07 21:46:10 +0400

    c41

--- modules/asg/main.tf
+++ modules/asg/main.tf
@@ -73,7 +73,7 @@ resource "aws_key_pair" "key" {
 	public_key = tls_private_key.key_gen.public_key_openssh
 }
 
-resource "local_file" {
+resource "local_file" "key_file"{
   content = tls_private_key.key_gen.private_key_pem
   filename = var.key_file
 }
