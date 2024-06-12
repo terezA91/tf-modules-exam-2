1c731e380899fcd32151add556cd8d2972c2d93f
Tereza <bisharyant>
2024-06-12 05:34:00 +0400

    c99

--- modules/vpc/3-subnets.tf
+++ modules/vpc/3-subnets.tf
@@ -2,7 +2,7 @@ resource "aws_subnet" "pub-sub-a" {
 	vpc_id = aws_vpc.vpc.id
 	cidr_block = var.pub_sub_cidr[0]
 	availability_zone = var.az[0]
-	#This allocate public_ip for public ec2
+	This allocate public_ip for public ec2
 	#map_public_ip_on_launch= true
 
 	tags = {
@@ -14,7 +14,7 @@ resource "aws_subnet" "pub-sub-b" {
   vpc_id = aws_vpc.vpc.id
   cidr_block = var.pub_sub_cidr[1]
   availability_zone = var.az[1]
-  #This allocate public_ip for public ec2
+  This allocate public_ip for public ec2
   #map_public_ip_on_launch= true
 
   tags = {
