f09d2d0c9cf852771b0596e5beeb4ed9c94e3827
Tereza <bisharyant>
2024-06-12 06:04:31 +0400

    c101

--- modules/load_balancer/user_data.sh
+++ modules/load_balancer/user_data.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
+/*
 sudo yum update -y
 sudo yum install -y httpd
 sudo systemctl start httpd
 sudo systemctl enable httpd
-echo "<h1>Hello World from $(hostname -f)</h1>" > /var/www/html/index.html
+*/
+
+//echo "<h1>Hello World from $(hostname -f)</h1>" > /var/www/html/index.html
+
+sudo apt update
+sudo apt install -y apache2
+sudo systemctl start apache2
+sudo systemctlenable apache2
+echo “Hello from $(hostname -f).Created by USERDATA in Terraform. > /var/www/html/index.html
