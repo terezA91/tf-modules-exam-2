54d4b0283ef40128c365b5dcc3db195f022614f8
Tereza <bisharyant>
2024-06-18 11:08:39 +0400

    Add two ls output

--- main.tf
+++ main.tf
@@ -40,8 +40,8 @@ module "s3" {
 
   # >>>Lambda portion
   trigger_lambda = var.trigger_lambda
-  lf_arn         = var.trigger_lambda ? module.lambda[0].lf_arn : "dsd"
-  lf_permission  = var.trigger_lambda ? module.lambda[0].lf_permission : "dsd"
+  lf_arn         = var.trigger_lambda ? module.lambda[0].lf_arn : module.lambda[0].lf_arn_2
+  lf_permission  = var.trigger_lambda ? module.lambda[0].lf_permission : module.lambda[0].lf_perm_2
   # >>>CloudFront portion
   cf_name       = var.with_cf ? module.cloudfront[0].cf_name : ""
   policy_for_cf = var.with_cf ? module.cloudfront[0].policy_for_cf : ""
--- modules/lambda_function/outputs.tf
+++ modules/lambda_function/outputs.tf
@@ -3,7 +3,17 @@ output "lf_arn" {
     description = "Arn of lambda function"
 }
 
+output "lf_arn_2" {
+    value = aws_lambda_function.tf-lambda-up.arn
+    description = "Arn of lambda function"
+}
+
 output "lf_permission" {
+    value = aws_lambda_permission.alp
+    description = "Name of the <aws_lambda_permission> resource"
+}
+
+output "lf_perm_2" {
     value = aws_lambda_permission.alp
     description = "Name of the <aws_lambda_permission> resource"
 }
